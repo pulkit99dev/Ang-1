75ff32296ac04acd501c633781a15bc911ff3dbe
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-05-31 15:50:34 +0530

    complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
+import { Todo } from "./todo";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'Learn Angular';
-  date = new Date();
-  name = 'Pulkit';
+  title = "Learning Angular";
+
+  todoValue: string = "";
+  list: Todo[] = [];
+
+  ngOnInit() {
+    this.list = [];
+    this.todoValue = "";
+  }
+
+  addItem() {
+    if (this.todoValue !== "") {
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false
+      };
+      this.list.push(newItem);
+    }
+    this.todoValue = "";
+  }
+
+  deleteItem(id: number) {
+    this.list = this.list.filter(item => item.id !== id);
+  }
 }
--- src/app/todo.ts
+++ src/app/todo.ts
@@ -1,3 +1,5 @@
 export interface Todo{
-    
+    id: number,
+    value: string,
+    isDone: boolean
 }
